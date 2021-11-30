726a7c9b91ca317b1c931892b69db32b3baa7e6f
Jesper Bengtsson <jesperbengtsson2@gmail.com>
2021-11-30 14:26:21 +0100

    Added column sizes

--- src/components/RecommendedBooking.js
+++ src/components/RecommendedBooking.js
@@ -40,10 +40,10 @@ export default function RecommendedBooking(props) {
 
     return (
         <Row className="mt-1 mb-1">
-            <Col>Seat: {props.slot.seat}</Col>
-            <Col>From: {props.slot.from.getHours()<10?"0"+props.slot.from.getHours():props.slot.from.getHours()}:{props.slot.from.getMinutes()<10?"0"+props.slot.from.getMinutes():props.slot.from.getMinutes()} </Col>
-            <Col>To: {props.slot.to.getHours()<10?"0"+props.slot.to.getHours():props.slot.to.getHours()}:{props.slot.to.getMinutes()<10?"0"+props.slot.to.getMinutes():props.slot.to.getMinutes()} </Col>
-            <Col><Button variant="contained" onClick={handleCreateBooking}>Book</Button></Col>
+            <Col className="xs md-3">Seat: {props.slot.seat}</Col>
+            <Col className="xs md-3">From: {props.slot.from.getHours()<10?"0"+props.slot.from.getHours():props.slot.from.getHours()}:{props.slot.from.getMinutes()<10?"0"+props.slot.from.getMinutes():props.slot.from.getMinutes()} </Col>
+            <Col className="xs md-3">To: {props.slot.to.getHours()<10?"0"+props.slot.to.getHours():props.slot.to.getHours()}:{props.slot.to.getMinutes()<10?"0"+props.slot.to.getMinutes():props.slot.to.getMinutes()} </Col>
+            <Col className="xs md-3"><Button variant="contained" onClick={handleCreateBooking}>Book</Button></Col>
         </Row>
     )
 }
